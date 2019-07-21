09e16663fbd4280e0f7dfc273e9d570f1dd02211
UddeshJain <jainuddesh35@gmail.com>
2019-07-21 22:51:56 +0530

    Added few more routes

--- routes/genres.js
+++ routes/genres.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Joi = require('@hapi/joi')
 const router = express.Router()
 
 const Genre = mongoose.model('Genre', new mongoose.Schema({
@@ -11,19 +12,42 @@ const Genre = mongoose.model('Genre', new mongoose.Schema({
     }
 }))
 
+function validateGenre(genre) {
+    const schema = {
+        name: Joi.string().required()
+    }
+    return Joi.validate(genre, schema)
+}
+
 router.get('/', async (req, res) => {
     const genres = await Genre.find()
     if (genres) {
         res.send(genres)
     } else {
-        res.send('Error....').status(400)   
+        res.status(400) .send('Record not found.')  
     }
 })
 
 router.post('/', async (req, res) => {
+    const { error } = validateGenre(req.body)
+    if (error) {
+        res.status(400).send(error.details[0].message)
+    }
     let genre = new Genre({ name: req.body.name })
-    let result = await genre.save()
-    res.send(result)
+    genre = await genre.save()
+    res.send(genre)
+})
+
+router.put('/:id', async (req, res) => {
+    // const { error } = validateGenre(req.body)
+    // if (error) {
+    //     res.status(400).send(error.details[0].message)
+    // }
+    const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true })
+    if (!genre) {
+        return res.status(400).send('The genre with the give ID is not found')
+    }
+    res.send(genre)
 })
 
 module.exports = router
