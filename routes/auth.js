7d67a88c744f68c6606bc1a987fdabd51f1be812
UddeshJain <jainuddesh35@gmail.com>
2019-08-02 21:02:58 +0530

    Added json web token support

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,21 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const Joi = require('@hapi/joi')
-const {User} = require('./users')
+const {User} = require('../models/user')
 const router = express.Router()
 
 router.post('/', async (req, res) => {
     const { error } = validate(req.body)
     if (error) return res.status(400).send(error.details[0].message)
-    let user = await User.findOne({ emal: req.body.email })
+    let user = await User.findOne({ email: req.body.email })
     if (!user) return res.status(400).send('Invalid email or password')
+
     const validPassword = await bcrypt.compare(req.body.password, user.password)
     if (!validPassword) return res.status(400).send('Invailid email or password')
-    res.send(true)
+
+    const token = jwt.sign({_id: user._id}, 'jsonSecret')
+    res.send(token)
 })
 
 function validate(req) {
